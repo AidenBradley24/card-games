45f6962061803d31f38fadec277a3f2ec391d1c1
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-12 18:57:28 -0400

    started menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,48 @@ import './App.css';
 
 import { PrimeReactProvider } from 'primereact/api';
 import * as BJ from './blackjack/Blackjack';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+
+
+const HomePage = () => (
+  <div className='home'>
+    <ul>
+      <li><NavLink to='/blackjack'>Blackjack</NavLink></li>
+    </ul>
+  </div>
+);
+
+const BlackjackPage = () => (
+  <BJ.BlackJack></BJ.BlackJack>
+);
+
+const Main = () => (
+  <body>
+    <Routes>
+      <Route path='/' Component={HomePage}></Route>
+      <Route path='/blackjack' Component={BlackjackPage}></Route>
+    </Routes>  
+  </body> 
+)
 
 function App() {
 
+  let topMenuItems = [
+    { label: "Home", url: "/" }
+  ];
+  
   return (
     <PrimeReactProvider>
       <div className="App">
         <header className="App-header">
-          <p>
-            Blackjack
-          </p>
+          <Menubar className='topmenu' model={topMenuItems} start={<p>Card Games</p>}/>
         </header>
-        <BJ.BlackJack></BJ.BlackJack>
+        <Main/>
         <footer className="App-footer">
-
         </footer>
       </div>
-    </PrimeReactProvider>
+    </PrimeReactProvider>    
   );
 }
 
